ebe32a118d2463bfe92bfa35c5150c8f43d6fd95
Asadsammar <masadsammar@gmail.com>
2024-03-25 18:39:29 +0700

    Practicum 2

--- src/app/component/button.tsx
+++ src/app/component/button.tsx
@@ -1,19 +1,39 @@
 'use client'
 export default function Tombol1(){
     function handleMouseOver(){
-        alert("Eits, mau mencet tombol ya?");
+        alert("Eits, want to press the button?");
       }
     function handleClick(){
-        alert("Tombol sudah ditekan!");
+        alert("The button has been pressed!");
     }
     
     return(
         <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded" 
         onClick={handleClick} 
-        onMouseLeave={() => {alert("Loh, kok sudah pergi!!!")}}
+        onMouseLeave={() => {alert("Oh, how come you're gone!!!")}}
         //onMouseOver={handleMouseOver}
         >
             Ini tombol
         </button>
     );
 }
+export function Tombol2({isiPesan, namaTombol}:any) {
+    return (
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        onClick={()=>alert(isiPesan)}
+      >
+        {namaTombol}
+      </button>
+    );
+  }
+  
+  /*export function Tombol3({isiPesan, namaTombol}:any){
+      return (
+          <button
+              className="bg-green-400 hover:bg-blue-700 text-white p-2 rounded"
+              onClick={(e)=>{e.stopPropagation; alert(isiPesan)}}>
+              {namaTombol}
+          </button>
+      );
+  }*/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Tombol_1 from "./component/button";
-
+import Image from "next/image";
+import Tombol1, { Tombol2,} from "./component/button";
 export default function Home() {
   return (
     <>
       <div className="container mx-auto">
         <h2>Kuis Kota</h2>
-        <Tombol_1 />
+        <Tombol1 />
       </div>
     </>
   );
